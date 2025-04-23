033e1341bb9c1eacc81e4cdeb28e9ad22d072271
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-04-23 10:32:58 +0200

    expose src

--- flake.nix
+++ flake.nix
@@ -11,43 +11,47 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      imports = [ inputs.treefmt-nix.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          system,
-          self',
-          ...
-        }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.nix-ros-overlay.overlays.default
-              inputs.gepetto.overlays.default
-            ];
-          };
-          checks = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
-          packages = {
-            default = self'.packages.agimus-msgs;
-            agimus-msgs = pkgs.rosPackages.humble.agimus-msgs.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./msg
-                  ./package.xml
-                ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, ... }:
+      {
+        systems = [ "x86_64-linux" ];
+        imports = [ inputs.treefmt-nix.flakeModule ];
+        flake.src = inputs.nixpkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = inputs.nixpkgs.lib.fileset.unions [
+            ./CMakeLists.txt
+            ./msg
+            ./package.xml
+          ];
+        };
+        perSystem =
+          {
+            lib,
+            pkgs,
+            system,
+            self',
+            ...
+          }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.nix-ros-overlay.overlays.default
+                inputs.gepetto.overlays.default
+              ];
+            };
+            checks = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+            packages = {
+              default = self'.packages.agimus-msgs;
+              agimus-msgs = pkgs.rosPackages.humble.agimus-msgs.overrideAttrs {
+                inherit (self) src;
               };
             };
+            treefmt.programs = {
+              deadnix.enable = true;
+              nixfmt.enable = true;
+            };
           };
-          treefmt.programs = {
-            deadnix.enable = true;
-            nixfmt.enable = true;
-          };
-        };
-    };
+      }
+    );
 }
