3298d8359d62920437a972d10836e05895a44c78
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-09 12:57:50 +0100

    update packaging

--- flake.nix
+++ flake.nix
@@ -13,49 +13,25 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } (
-      { lib, self, ... }:
+      { lib, ... }:
       {
         systems = import inputs.systems;
         imports = [
           inputs.gazebros2nix.flakeModule
-          { gazebros2nix-pkgs.overlays = [ self.overlays.default ]; }
-        ];
-        flake.overlays.default =
-          _final: prev:
-          let
-            scope = _ros-final: ros-prev: {
-              agimus-msgs = ros-prev.agimus-msgs.overrideAttrs {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./CMakeLists.txt
-                    ./action
-                    ./msg
-                    ./package.xml
-                  ];
-                };
+          {
+            gazebros2nix.rosOverrides.agimus-msgs = _final: _ros-final: {
+              src = lib.fileset.toSource {
+                root = ./.;
+                fileset = lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./action
+                  ./msg
+                  ./package.xml
+                ];
               };
             };
-          in
-          {
-            rosPackages = prev.rosPackages // {
-              humble = prev.rosPackages.humble.overrideScope scope;
-              jazzy = prev.rosPackages.jazzy.overrideScope scope;
-              kilted = prev.rosPackages.kilted.overrideScope scope;
-              rolling = prev.rosPackages.rolling.overrideScope scope;
-            };
-          };
-        perSystem =
-          { pkgs, ... }:
-          {
-            packages = lib.filterAttrs (_n: v: v.meta.available && !v.meta.broken) (rec {
-              default = rolling-agimus-msgs;
-              humble-agimus-msgs = pkgs.rosPackages.humble.agimus-msgs;
-              jazzy-agimus-msgs = pkgs.rosPackages.jazzy.agimus-msgs;
-              kilted-agimus-msgs = pkgs.rosPackages.kilted.agimus-msgs;
-              rolling-agimus-msgs = pkgs.rosPackages.rolling.agimus-msgs;
-            });
-          };
+          }
+        ];
       }
     );
 }
